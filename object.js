25e8c541b63011b07efd296be87102969a93fd44
Shohei Kikuchi <killchance2256@gmail.com>
2020-09-24 01:57:03 +0900

    objects

--- object.js
+++ object.js
@@ -1,8 +1,40 @@
 {
-  // const score1 = 80;
-  // const score2 = 90;
-  // const score3 = 40;
+  // const otherScores = [10, 20];
+  const scores =[80,90,40,70];
 
-  const scores =[80,90,40];
-  console.log(scores);
+  scores.forEach((score,index)=>{
+    console.log(`Score ${index}:${score}`);
+  });
+
+  // const [a, b, c, d] = scores;
+  // console.log(a);
+  // console.log(b);
+  // console.log(c);
+  // console.log(d);
+  // const [a, b,...others] = scores;
+  // console.log(a);
+  // console.log(b);
+  // console.log(others);
+  // let x = 30;
+  // let y = 70;
+  // [x, y] = [y, x ];
+  // console.log(x);
+  // console.log(y);
+
+
+  // console.log(scores);
+
+  // function sum(a, b) {
+  //   console.log(a + b);
+  // }
+  // sum(...otherScores);
+  // scores.push(60,50); 末尾に追加
+  // scores.shift();　先頭を削除
+  // scores.splice(1,2,30,80);
+  
+
+  // for (let i = 0; i < scores.length; i++) {
+  // console.log(`Score ${i}: ${scores[i]}`);
+  // }
 }
+
