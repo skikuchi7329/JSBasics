a84368934acae4cdeb7c88603902edd53bed5d27
Shohei Kikuchi <killchance2256@gmail.com>
2020-09-24 17:23:31 +0900

    class

--- object.js
+++ object.js
@@ -88,3 +88,169 @@
   console.log(points[1].y);
 }
 
+{
+  // let x = [1, 2];
+  // let y = x;
+  // x[0] = 5;
+  // console.log(x);
+  // console.log(y);
+
+  let x = [1, 2];
+  let y = [...x];
+  x[0] = 5;
+  console.log(x);
+  console.log(y);
+}
+
+{
+  const str = 'hello';
+
+  // console.log(str.length);
+
+  // console.log(str.substring(2, 4));
+  console.log(str[1]);
+}
+
+{
+  const d = [2019, 11 , 14];
+
+  console.log(d.join('/'));
+
+  const t = '17:08:24';
+  // console.log(t.split(':'));
+
+  const [hour, minute, second] = t.split(':');
+  console.log(hour);
+  console.log(minute);
+  console.log(second);
+}
+
+{
+  const scores = [10, 3, 9];
+
+  let sum = 0;
+
+  scores.forEach(score => {
+    sum += score;
+  });
+
+  const avg = sum / scores.length;
+
+  console.log(sum);
+  console.log(avg);
+
+  // console.log(Math.floor(avg));
+  // console.log(Math.ceil(avg));
+  // console.log(Math.round(avg));
+  // console.log(avg.toFixed(3));
+
+  console.log(Math.floor(Math.random() * 3));
+
+  console.log(Math.floor(Math.random() * 6) + 1);
+}
+
+{
+  const d = new Date(2020,8);
+  d.setHours(10, 20, 30);
+  d.setDate(24);
+  d.setDate(d.getDate() + 3);
+
+  console.log(d);
+
+  console.log(`${d.getMonth() + 1}月  ${d.getDate()}日`);
+}
+
+// {
+//   // alert('hello');
+//   const answer = confirm('削除しますか？');
+//   if(answer) {
+//     console.log('削除しました');
+//   } else {
+//     console.log('キャンセルしました');
+//   }
+// }
+
+// {
+//   let i = 0;
+//   function showTime() {
+//     console.log(new Date());
+//     i++;
+//     if (i > 2) {
+//       clearInterval(intervalId);
+//     }
+//   }
+  
+//   const intervalId = setInterval(showTime, 1000);
+
+// }
+
+// {
+  
+{
+  const name = 'taguchi';
+  // const name = 5;
+
+  try {
+    console.log(name.toUpperCase());
+
+  } catch (e) {
+    console.log(e);
+  }
+
+  console.log('Finish!');
+}
+
+
+
+{
+  class Post {
+    constructor(text) {
+      this.text = text;
+      this.likeCount = 0;
+    }
+  
+
+    show() {
+      console.log(`${this.text} - ${this.likeCount} likes`);
+    }
+
+    like() {
+      this.likeCount++;
+      this.show();
+    }
+
+    
+
+  }
+  class SponsoredPost extends Post {
+    constructor(text, sponsor) {
+      super(text);
+      this.sponsor = sponsor;
+    }
+  
+
+    show() {
+      console.log(`${this.text} - ${this.likeCount} likes`);
+      console.log(`... sponsored by ${this.sponsor}`);
+    }
+
+    like() {
+      this.likeCount++;
+      this.show();
+    }
+
+    
+
+  }
+
+
+
+  const posts = [
+    new Post('JavaScript勉強中'),
+    new Post('プログラミング'),
+    new SponsoredPost('３分動画でマスターしよう', 'dotinstall'),
+    
+  ];
+ posts[2].like();
+
+}
